1f11141cd1bfb4a75ec50c63994fbf4671fedbdf
Jacqueline-Ong <sparks_of_light@hotmail.com>
2022-11-14 17:37:16 +0800

    Update app.js for cyclic deployment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
     res.render('Home');
   });
@@ -9,4 +11,3 @@ app.listen(3000, function()
   {console.log("Server started on port 3000");}
 );
 
-app.set("view engine", "ejs");
